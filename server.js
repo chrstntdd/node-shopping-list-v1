ed5e335e988258eb485d0662ab6ad418e3478f11
Christian Todd <christiantodd27@icloud.com>
2017-04-20 18:54:59 -0400

    complete challenge.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const {ShoppingList} = require('./models');
+const {ShoppingList, Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -18,11 +18,19 @@ ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
+Recipes.create('peanut butter and jelly', ['crunchy peanut butter', 'strawberry jelly', 'wheat bread']);
+Recipes.create('coffee', ['ground coffee beans', '205 degree water']);
+Recipes.create('fried rice', ['brown rice', 'bacon', 'eggs', 'turkey sausage', 'salt', 'pepper']);
+
 // when the root of this router is called with GET, return
 // all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
 });
+
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
+})
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
